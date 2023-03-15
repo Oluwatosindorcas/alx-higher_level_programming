e6270c45b7c3787eac57c5f342083356f024083c
Oluwatosindorcas <Oluwatosindorcas68@gmail.com>
2023-03-15 16:22:16 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-const newDict = {};
-for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [];
+
+const dict = require('./101-data').dict;
+const res = {};
+for (const item in dict) {
+  if (!(dict[item] in res)) {
+    res[dict[item]] = [item];
+  } else {
+    res[dict[item]].push(item);
   }
-  newDict[dict[key]].push(key);
 }
-console.log(newDict);
+console.log(res);
